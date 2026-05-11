cce7982b86ec730e81fe5f977919d30f359100a5
Sian <sian@Sians-MacBook-Pro.local>
2026-05-11 14:21:04 -0700

    updated entity

--- OverTime/Database/DatabaseManager.swift
+++ OverTime/Database/DatabaseManager.swift
@@ -13,7 +13,7 @@ class DatabaseManager : DatabaseManagerProtocol{
     init(containerName: String = "OverTime"){
         persistentContainer = NSPersistentContainer(name: containerName)
         persistentContainer.loadPersistentStores{ _ , error in
-            if let error {fatalError("CoreData failed : \(error)")}
+            if let _=error {fatalError("CoreData failed : \(error)")}
         }
     }
     private var context : NSManagedObjectContext{
